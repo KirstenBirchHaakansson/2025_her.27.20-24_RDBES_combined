6ce46832550a4a662c9e6e94665705c23b889afe
Kirsten Birch Håkansson, DTU Aqua <kih@aqua.dtu.dk>
2025-03-09 16:27:15 +0100

    Update data_C1_read_in_yellow_sheets.r

--- data_scripts/data_C1_read_in_yellow_sheets.r
+++ data_scripts/data_C1_read_in_yellow_sheets.r
@@ -1,25 +1,23 @@
 
-library(sqldf)
+# library(sqldf)
 library(dplyr)
 library(knitr)
+library(icesTAF)
+
+mkdir("data")
 
 options("scipen" = 1000, digits = 4)
 
 type <- "v1"
 
-year <- 2023
+year <- 2024
 
 # File names 2024
-dnkFile <- "DC_Annex_HAWG3 her.27.20-24 template_DNK_2023.xls"
-gerFile <- "2024_DC_Annex_7.1.3_HAWG_Template_her.27.20-24_update.xls"
-polFile <- "2024 DC HAWG her.27.20-24 PL_v2.xls"
-sweFile <- "DC_Annex_HAWG3_SWE_her.27.20-24_2023_v2.xls"
-norFile <- "DC_Annex_7.1.3. HAWG Template_her_20-24_NOR2023_v2.xls"
-
-# dnkFile_2021 <- "DC_Annex_7.1.3. HAWG Template_her_20-24_DNK_HER_2021.xls"
-# gerFile_2021 <- "2a_2022 DC HAWG her.27.20-24 DE AC4_GER 2021_Landings by gear.xls"
-# sweFile_2021 <- "DC_Annex_7.1.3. HAWG_SWE_her.27.20-24_2021.xls"
-
+dnkFile <- "DC_Annex_HAWG3 her.27.20-24 template_DNK_2024.xlsx"
+gerFile <- "2025_DC_Annex_HAWG3 her.27.20-24_2024_GER.xlsx"
+polFile <- "PL_DC_Annex_HAWG3 her.27.20-24_PL.xlsx"
+sweFile <- "SE_2025 DC HAWG her.27.20-24 YellowSheet_v2.xlsx"
+norFile <- "NO_DC_Annex_HAWG3 her.27.20-24_NOR2024.xlsx"
 
 
 # Read in data ----
@@ -28,58 +26,37 @@ norFile <- "DC_Annex_7.1.3. HAWG Template_her_20-24_NOR2023_v2.xls"
 dnk <-
   read_in_canum(
     paste("boot/data/", dnkFile, sep = ""),
-    sheets = c(6, 7, 8, 9, 10, 11, 12, 13, 14, 15),
+    sheets = c(6:22),
     noAgeClasses = 9
   )
 
 ger <-
   read_in_canum(
     paste("boot/data/", gerFile, sep = ""),
-    sheets = c(7:16),
+    sheets = c(7:23),
     noAgeClasses = 9
   )
 
 pol <-
   read_in_canum(
     paste("boot/data/", polFile, sep = ""),
-    sheets = c(8:15),
+    sheets = c(8:10),
     noAgeClasses = 9
   )
 
 swe <-
   read_in_canum(
     paste("boot/data/", sweFile, sep = ""),
-    sheets = c(6:18),
+    sheets = c(6:22),
     noAgeClasses = 9
   )
 
 nor <-
   read_in_canum(
     paste("boot/data/", norFile, sep = ""),
-    sheets = c(5:11),
+    sheets = c(6),
     noAgeClasses = 9
   )
-
-# dnk_2021 <-
-#   read_in_canum(
-#     paste(rawDataPath_2021, dnkFile_2021, sep = ""),
-#     sheets = c(6, 7, 8, 9, 10, 11, 12, 13, 14),
-#     noAgeClasses = 9
-#   )
-# 
-# ger_2021 <-
-#   read_in_canum(
-#     paste(rawDataPath_2021, gerFile_2021, sep = ""),
-#     sheets = c(7:17),
-#     noAgeClasses = 9
-#   )
-# 
-# swe_2021 <-
-#   read_in_canum(
-#     paste(rawDataPath_2021, sweFile_2021, sep = ""),
-#     sheets = c(6:12),
-#     noAgeClasses = 9
-#   )
 
 
 ## Read in samples ----  
@@ -87,35 +64,35 @@ nor <-
 dnkS <-
   read_in_samples(
     paste("boot/data/", dnkFile, sep = ""),
-    sheets = c(6, 7, 8, 9, 10, 11, 12, 13, 14, 15),
+    sheets = c(6:22),
     firstLineSamples = 27
   )
 
 gerS <-
   read_in_samples(
     paste("boot/data/", gerFile, sep = ""),
-    sheets = c(7:16),
+    sheets = c(7:23),
     firstLineSamples = 27
   )
 
 polS <-
   read_in_samples(
     paste("boot/data/", polFile, sep = ""),
-    sheets = c(8:15),
+    sheets = c(8:10),
     firstLineSamples = 27
   )
 
 sweS <-
   read_in_samples(
     paste("boot/data/", sweFile, sep = ""),
-    sheets = c(6:18),
+    sheets = c(6:22),
     firstLineSamples = 27
   )
 
 norS <-
   read_in_samples(
     paste("boot/data/", norFile, sep = ""),
-    sheets = c(5:11),
+    sheets = c(6),
     firstLineSamples = 27
   )
 
@@ -148,95 +125,49 @@ norC <-
                 noAreas = 29)
 
 
-## Read in length ----
-
-# dnkL <-
-#   read_in_length(
-#     paste(rawDataPath, dnkFile, sep = ""),
-#     sheets = c(6, 8, 10, 12, 14, 16, 18),
-#     firstLineLength = 10
-#   )
-# 
-# gerL <-
-#   read_in_length(
-#     paste(rawDataPath, gerFile, sep = ""),
-#     sheets = c(5:6),
-#     firstLineLength = 10
-#   )
-# 
-# polL <-
-#   read_in_length(
-#     paste(rawDataPath, polFile, sep = ""),
-#     sheets = c(5),
-#     firstLineLength = 10
-#   )
-# 
-# sweL <-
-#   read_in_length(
-#     paste(rawDataPath, sweFile, sep = ""),
-#     sheets = c(5:9),
-#     firstLineLength = 10
-#   )
-# 
-# norL <-
-#   read_in_length(
-#     paste(rawDataPath, norFile, sep = ""),
-#     sheets = c(5),
-#     firstLineLength = 10
-#   )
-
-
 ## Read in landings per square from area_official ----
 
 
 dnkR <-
   read_in_areaOfficial(
     path = paste("boot/data/", dnkFile, sep = ""),
-    sheets = c(16),
+    sheets = c(23),
     noRectangles = 1182
   )
 
 gerR <-
   read_in_areaOfficial(
     path = paste("boot/data/", gerFile, sep = ""),
-    sheets = c(17),
+    sheets = c(24),
     noRectangles = 1182
   )
 
 polR <-
   read_in_areaOfficial(
     path = paste("boot/data/", polFile, sep = ""),
-    sheets = c(16),
+    sheets = c(11),
     noRectangles = 1182
   )
 
 sweR <-
   read_in_areaOfficial(
     path = paste("boot/data/", sweFile, sep = ""),
-    sheets = c(19),
+    sheets = c(23),
     noRectangles = 1182
   )
 
 norR <-
   read_in_areaOfficial(
     path = paste("boot/data/", norFile, sep = ""),
-    sheets = c(12),
+    sheets = c(7),
     noRectangles = 1183
   )
 
 # Combine and recode ----
 ## Canum ----
 
-# canum_2021 <- rbind(dnk_2021, ger_2021, swe_2021)
-# 
-# names(canum_2021)
-# 
-# # Remove SWE samples- agrred by Valerio
-# 
-# canum_2021 <- subset(canum_2021, !((ctry == "Sweden" & year == 2021 & area == "SD 20" & quarter == 4)))
-# canum_2021 <- subset(canum_2021, !((ctry == "Sweden" & year == 2021 & area == "SD 21" & quarter == 4)))
 
-canum <- rbind(dnk, ger, pol, swe, nor) #, canum_2021)
+canum <- rbind(dnk, ger, pol, swe, nor)
 
 unique(canum$ctry)
 
@@ -277,61 +208,18 @@ unique(canum$area)
 
 distinct(canum, area, ctry, fleet)
 
-canum$subFleet <-
-  ifelse(
-    canum$area %in% c("27.3.a.20", "27.3.a.21") &
-      canum$fleet %in% c("C", "Fleet-C", "> 32 mm", "Fleet-A"),
-    "C",
-    ifelse(
-      canum$area %in% c("27.3.a.20", "27.3.a.21") &
-        canum$fleet %in% c("C - purse seine"),
-      "C - purse seine",
-      ifelse(
-        canum$area %in% c("27.3.a.20", "27.3.a.21") &
-          canum$fleet %in% c("C - passive"),
-        "C - passive",
-        ifelse(
-          canum$area %in% c("27.3.a.20", "27.3.a.21") &
-            canum$fleet %in% c("C - Other"),
-          "C - other",
-          ifelse(
-            canum$area %in% c("27.3.a.20", "27.3.a.21") &
-              canum$fleet %in% c("C - trawl"),
-            "C - trawl",
-    ifelse(
-      canum$area %in% c("27.3.a.20", "27.3.a.21") &
-        canum$fleet %in% c("D", "Fleet-D", "< 32mm", "< 32 mm"),
-      "D",
-      ifelse(
-        canum$area %in% c("27.3.c.22", "27.3.b.23", "27.3.d.24", "all") &
-          canum$fleet %in% c("F - active", "F Trawl"),
-        "F - active",
-        ifelse(
-          canum$area %in% c("27.3.c.22", "27.3.b.23", "27.3.d.24", "all") &
-            canum$fleet %in% c("F - passive", "F- passive", "F Gillnet", "F Trapnet") ,
-          "F - passive", canum$fleet
-      ))
-    )
-  )))))
+canum$subFleet <- canum$fleet
+
+canum$fleet[canum$area %in% c("27.3.a.20", "27.3.a.21") &
+              canum$subFleet %in% c("passive", "purse seine", "trawl >= 32mm")] <- "C"
+
+canum$fleet[canum$area %in% c("27.3.a.20", "27.3.a.21") &
+              canum$subFleet %in% c("trawl < 32mm")] <- "D"
+
+canum$fleet[canum$area %in% c("27.3.c.22", "27.3.b.23", "27.3.d.24")] <- "F"
+
 
 distinct(canum, ctry, fleet, subFleet, area)
-
-canum$fleet <-
-  ifelse(
-    canum$area %in% c("27.3.a.20", "27.3.a.21") &
-      canum$fleet %in% c("C", "Fleet-C", "> 32 mm", "Fleet-A", "C - purse seine", "C - passive", "C - trawl", "C - Other"),
-    "C",
-    ifelse(
-      canum$area %in% c("27.3.a.20", "27.3.a.21") &
-        canum$fleet %in% c("D", "Fleet-D", "< 32mm", "< 32 mm"),
-      "D",
-      ifelse(
-        canum$area %in% c("27.3.c.22", "27.3.b.23", "27.3.d.24", "all"),
-        "F",
-        canum$fleet
-      )
-    )
-  )
 
 distinct(canum, area, fleet)
 
@@ -370,7 +258,7 @@ canum <- subset(canum,!is.na(wr))
 year_dummy <- distinct(canum, year)
 ctry <- distinct(canum, ctry)
 sppName <- distinct(canum, sppName)
-fleet <- distinct(canum, fleet, area)
+fleet <- distinct(canum, fleet, subFleet, area)
 wr <- distinct(canum, wr)
 quarter <- distinct(canum, quarter)
 
@@ -378,7 +266,7 @@ dummy <-
   sqldf("select * from year_dummy, ctry, sppName, fleet, wr, quarter")
 
 canum0 <-
-  canum[, c("year", "ctry", "sppName", "fleet", "area", "wr", "quarter")]
+  canum[, c("year", "ctry", "sppName", "fleet", "subFleet", "area", "wr", "quarter")]
 dummy1 <- setdiff(dummy, canum0)
 
 canum1 <- bind_rows(canum, dummy1)
@@ -395,9 +283,9 @@ samp <- rbind(dnkS, gerS, polS, sweS, norS)
 
 unique(samp$noSample)
 
-samp$noSample <- ifelse(samp$noSample == -9, 0, samp$noSample)
-samp$noLength <- ifelse(samp$noLength == -9, 0, samp$noLength)
-samp$noAge <- ifelse(samp$noAge == -9, 0, samp$noAge)
+samp$noSample <- ifelse(is.na(samp$noSample), 0, samp$noSample)
+samp$noLength <- ifelse(is.na(samp$noLength), 0, samp$noLength)
+samp$noAge <- ifelse(is.na(samp$noAge), 0, samp$noAge)
 
 unique(samp$noSample)
 
@@ -443,105 +331,22 @@ samp$area <-
     )
   )
 
-unique(samp$area)
+distinct(samp, area, ctry, fleet)
 
-samp$subFleet <-
-  ifelse(
-    samp$area %in% c("27.3.a.20", "27.3.a.21") &
-      samp$fleet %in% c("C", "Fleet-C", "> 32 mm", "Fleet-A"),
-    "C",
-    ifelse(
-      samp$area %in% c("27.3.a.20", "27.3.a.21") &
-        samp$fleet %in% c("C - purse seine"),
-      "C - purse seine",
-      ifelse(
-        samp$area %in% c("27.3.a.20", "27.3.a.21") &
-          samp$fleet %in% c("C - passive"),
-        "C - passive",
-        ifelse(
-          samp$area %in% c("27.3.a.20", "27.3.a.21") &
-            samp$fleet %in% c("C - Other"),
-          "C - other",
-          ifelse(
-            samp$area %in% c("27.3.a.20", "27.3.a.21") &
-              samp$fleet %in% c("C - trawl"),
-            "C - trawl",
-    ifelse(
-      samp$area %in% c("27.3.a.20", "27.3.a.21") &
-        samp$fleet %in% c("D", "Fleet-D", "< 32mm", "< 32 mm"),
-      "D",
-      ifelse(
-        samp$area %in% c("27.3.c.22", "27.3.b.23", "27.3.d.24", "all") &
-          samp$fleet %in% c("F - active", "F Trawl"),
-        "F - active",
-        ifelse(
-          samp$area %in% c("27.3.c.22", "27.3.b.23", "27.3.d.24", "all") & samp$ctry != "Germany" &
-            samp$fleet %in% c("F - passive", "F- passive", "F Gillnet", "F Trapnet") ,
-          "F - passive", samp$fleet
-      ))
-    )
-  )))))
+samp$subFleet <- samp$fleet
 
-distinct(canum, fleet, subFleet, area)
+samp$fleet[samp$area %in% c("27.3.a.20", "27.3.a.21") &
+              samp$subFleet %in% c("passive", "purse seine", "trawl >= 32mm")] <- "C"
 
-samp$fleet <-
-  ifelse(
-    samp$area %in% c("27.3.a.20", "27.3.a.21") &
-      samp$fleet %in% c("C", "Fleet-C", "> 32 mm", "Fleet-A", "C - purse seine", "C - passive", "C - trawl", "C - Other"),
-    "C",
-    ifelse(
-      samp$area %in% c("27.3.a.20", "27.3.a.21") &
-        samp$fleet %in% c("D", "Fleet-D", "< 32mm", "< 32 mm"),
-      "D",
-      ifelse(
-        samp$area %in% c("27.3.c.22", "27.3.b.23", "27.3.d.24"),
-        "F",
-        samp$fleet
-      )
-    )
-  )
+samp$fleet[samp$area %in% c("27.3.a.20", "27.3.a.21") &
+              samp$subFleet %in% c("trawl < 32mm")] <- "D"
+
+samp$fleet[samp$area %in% c("27.3.c.22", "27.3.b.23", "27.3.d.24")] <- "F"
+
+
+distinct(samp, ctry, fleet, subFleet, area)
 
 distinct(samp, area, fleet)
-
-## length ----
-
-# 
-# length<-rbind(dnkL,gerL,polL,sweL,norL)
-# 
-# unique(length$ctry)
-# 
-# unique(length$sppName)
-# 
-# length$sppName<-ifelse(length$sppName=="Herring (Clupea harengus)", "Herring", length$sppName)
-# 
-# unique(length$sppName)
-# 
-# unique(length$year)
-# 
-# unique(length$area)
-# 
-# length$area<-ifelse(length$area %in% c("SD20","SD 20","IIIaN"), "27.3.a.20",
-#                    ifelse(length$area %in% c("SD21","SD 21","IIIaS"), "27.3.a.21", 
-#                    ifelse(length$area %in% c("SD22","SD 22","BAL22"), "27.3.c.22",
-#                    ifelse(length$area %in% c("SD23","SD 23","BAL23"), "27.3.b.23",
-#                    ifelse(length$area %in% c("SD24","SD 24","BAL24"), "27.3.d.24", length$area)))))
-# unique(length$area)
-# 
-# distinct(length, area, fleet)
-# 
-# length$fleet<-ifelse(length$area %in% c("27.3.a.20","27.3.a.21") & length$fleet %in% c("C","Fleet-C","> 32 mm", "C-fleet", "Fleet C"), "C",
-#                     ifelse(length$area %in% c("27.3.a.20","27.3.a.21") & length$fleet %in% c("D","Fleet-D","< 32mm","< 32 mm", "D-fleet"), "D",
-#                     ifelse(length$area %in% c("27.3.c.22","27.3.b.23","27.3.d.24"),"F", length$fleet)))
-# 
-# distinct(length, area, fleet)
-# 
-# ##Handling units
-# 
-# length$clnum[is.na(length$clnum)]<-0
-# 
-# unique(length$clnum_unit)
-# 
-# length$clnum_1000<-ifelse(length$length_unit=="(millions)", length$clnum*1000, length$clnum)
 
 
 ## Catch ----
@@ -565,9 +370,9 @@ catch$ctry[catch$ctry == "POLAND"] <- "Poland"
 unique(catch$area)
 
 catch$area <- ifelse(
-  catch$area == "3a/Skagerak",
+  catch$area == "3an/Skagerrak",
   "27.3.a.20",
-  ifelse(catch$area == "3a/Kategat", "27.3.a.21", catch$area)
+  ifelse(catch$area == "3as/Kattegat", "27.3.a.21", catch$area)
 )
 
 
