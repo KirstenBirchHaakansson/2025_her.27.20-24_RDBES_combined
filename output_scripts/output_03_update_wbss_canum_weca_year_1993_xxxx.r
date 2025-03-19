03b7deab6e537e1f3f6e8eb991a45cff765cfb2c
Kirsten Birch Håkansson, DTU Aqua <kih@aqua.dtu.dk>
2025-03-19 15:06:54 +0100

    Updated years

--- output_scripts/output_03_update_wbss_canum_weca_year_1993_xxxx.r
+++ output_scripts/output_03_update_wbss_canum_weca_year_1993_xxxx.r
@@ -4,8 +4,8 @@ library(dplyr)
 library(knitr)
 library(readxl)
 
-year_last <- 2022
-year_to_update <- c(2023) # only works with a single year
+year_last <- 2023
+year_to_update <- c(2024) # only works with a single year
 
 # Old data
 
